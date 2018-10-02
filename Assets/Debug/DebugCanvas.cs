7f8142dc82fd524b29be392c2f751c62482bf789
GamerWang <GamerWang@outlook.com>
2018-10-01 21:10:33 -0600

    new note track touch function complete

---
+++ Assets/Debug/DebugCanvas.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DebugCanvas : MonoBehaviour {
+    // singleton instance
+    public static DebugCanvas instance;
+
+    // public references & variables
+    public Text debugText;
+
+    // public functions
+    public static void _PrintText(string str)
+    {
+        instance.debugText.text += "\n";
+        instance.debugText.text += str;
+    }
+
+    public void PrintText(string str)
+    {
+        debugText.text += "\n";
+        debugText.text += str;
+    }
+
+    // Use this for initialization
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
--- Assets/Scripts/TrebelManager.cs
+++ Assets/Scripts/TrebelManager.cs
@@ -21,6 +21,8 @@ public class TrebelManager : MonoBehaviour {
             // DEBUG
             SoundManager.instance.PlaySingleKey("C4");
             Debug.Log("TrebelManager: DragStart: " + Time.fixedTime);
+            //DebugCanvas.instance.PrintText("TrebelManager: DragStart: " + Time.fixedTime);
+            //DebugCanvas._PrintText("TrebelManager: DragStart: " + Time.fixedTime);
         }
     }
 
@@ -43,6 +45,14 @@ public class TrebelManager : MonoBehaviour {
             if (Input.touchCount > 0 || Input.GetMouseButton(0))
             {
                 movingNote.SetActive(true);
+                // Mouse controller
+                // Touch controller
+                if(Input.touchCount > 0)
+                {
+                    var touch = Input.GetTouch(0);
+                    var touchPosition = touch.position;
+                    movingNote.GetComponent<RectTransform>().anchoredPosition = touchPosition;
+                }
             }
             else
             {
